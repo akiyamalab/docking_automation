385db50a05b5b886ba9aa1b86ef920c50d50b0ba
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2024-02-18 15:48:48 +0000

    updating variable names

--- count.py
+++ count.py
@@ -1,11 +1,14 @@
+"""
+python count.py [SDF] [number of subfiles]
+"""
 import sys
 input_file_0=open(sys.argv[1],'r')
 lines=input_file_0.readlines()
-p=0
-h=sys.argv[2]
+n_cmpds=0 # number of compounds in the SDF
+n_subfiles=sys.argv[2]
 for line in lines:
     if line[0] == '$':
-        p=p+1
-n=int(p/int(h))
-print(n)
+        n_cmpds=n_cmpds+1
+n_cmpds_per_subfile=int(n_cmpds/int(n_subfiles))
+print(n_cmpds_per_subfile)
 #print(p)
